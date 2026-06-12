f2ad5ad8e46f6078af725be016af19c26ae2f4f2
44younes2020-dev <44younes2020@gmail.com>
2026-06-12 15:45:24 +0100

    Update mail_info.py

--- tools/mail_info.py
+++ tools/mail_info.py
@@ -126,7 +126,7 @@ def main():
         try:
             choice = int(input('\033[31mChoice >> \033[0m'))
             if choice == 0:
-                os.system('python cyb3rtech.py')
+                os.system('python khaled.py')
                 break
             elif choice == 1:
                 email_info()
